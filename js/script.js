9018540b765cb8b08fceac7aefb81c787cb6f4b2
Ihsan <san@Ihsans-MacBook-Air.local>
2024-01-27 00:22:29 +0700

    nambahin fitur search

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-// toggle class active
+// toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 //  ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// klik di luar sidebar untuk menghilangkan nav
+// toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik di luar untuk menghilangkan element
 const menu = document.querySelector("#hamburger-menu");
+const search = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
